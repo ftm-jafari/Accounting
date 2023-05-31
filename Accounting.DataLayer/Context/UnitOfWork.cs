e738efd7abc634f95dd6f62d91626b35768f3a22
ftm-jafari <100162308+ftm-jafari@users.noreply.github.com>
2023-05-31 13:47:01 +0330

    Add project files.

---
+++ Accounting.DataLayer/Context/UnitOfWork.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Accounting.DataLayer.Context
+{
+    public class UnitOfWork:IDisposable
+    {
+        Accounting_DBEntities db = new Accounting_DBEntities();
+
+        public void Dispose()
+        {
+           db.Dispose();
+        }
+    }
+}
---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,32 @@
+﻿using Accounting.DataLayer;
+using Accounting.DataLayer.Repositories;
+using Accounting.DataLayer.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Accounting_DBEntities db = new Accounting_DBEntities();
+            //ICustomerRepository customer = new CustomerRepository(db);
+
+            //Customers AddCustomer = new Customers()
+            //{
+            //    FullName= "معین خبیری",
+            //    CustomerImage = "NoPhoto",
+            //    Mobile = "09130025463"
+            //};
+
+            //customer.InsertCustomer(AddCustomer);
+            ////customer.Save();
+
+            //var list = customer.GetAllCustomers();
+        }
+    }
+}
